60d1bb9727628f4c55107195114b9b011541c522
psidae <akson.rill@gmail.com>
2020-09-08 16:19:23 +0300

    Add slim template language

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-has_person_name
+  has_person_name
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
